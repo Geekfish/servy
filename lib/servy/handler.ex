214f0ae3cd213001e3446c9ccfdd67eebc877912
Eleni Lixourioti <contact@eleni.co>
2019-03-05 19:22:04 +0000

    Patter match on paths, url rewrite

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,13 +1,50 @@
+require Logger
+
 defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
+    |> emojify
     |> format_response
   end
 
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{ conv | path: "/#{thing}/#{id}" }
+  end
+
+  def rewrite_path_captures(conv, nil), do: conv
+
+  def track(%{status: 404, path: path} = conv) do
+    Logger.warn "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
+
   def log(conv), do: IO.inspect(conv)
+
+  def emojify(%{status: 200} = conv) do
+    beer_for_bears = String.duplicate("🍻", 5)
+    %{conv | resp_body: beer_for_bears <> "\n" <> conv.resp_body <> "\n" <> beer_for_bears}
+  end
+
+  def emojify(conv), do: conv
 
   def parse(request) do
     [method, path, _] =
@@ -16,28 +53,44 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, status: nil, resp_body: ""}
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Liöns, Tigers"}
+  end
+
+  def route(%{method: "GET", path: "/bears/" <> bear_id} = conv) do
+    %{conv | status: 200, resp_body: "Bear ##{bear_id}"}
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> bear_id} = conv) do
+    Logger.error "Someone tried to remove a bear :("
+    %{conv | status: 403, resp_body: "Bear ##{bear_id} cannot be removed!"}
+  end
+
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
   def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
-
-  def route(conv, "GET", "/wildthings") do
-    %{conv | resp_body: "Bears, Liöns, Tigers"}
-  end
-
-  def route(conv, "GET", "/bears") do
-    %{conv | resp_body: "Teddy, Smokey, Paddington"}
-  end
-
-  def route(conv, "GET", _) do
-    %{conv | resp_body: "Default response"}
+    %{conv | status: 404, resp_body: "Path #{conv.path} not found"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
@@ -70,6 +123,50 @@ IO.puts(response)
 
 request = """
 GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bears?id=1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
