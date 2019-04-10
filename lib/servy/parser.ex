ca0fa7cf758f776b9b2c993db148f20bf7ff1873
Eleni Lixourioti <contact@eleni.co>
2019-04-10 23:16:39 +0100

    Reimplement header parsing with reduce

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -6,7 +6,7 @@ defmodule Servy.Parser do
     [request_line | header_lines] = String.split(top, "\n")
     [method, path, _] = String.split(request_line, " ")
 
-    headers = parse_headers(header_lines, %{})
+    headers = parse_headers(header_lines)
     params = parse_params(headers["Content-Type"], params_string)
 
     IO.inspect(header_lines)
@@ -14,13 +14,12 @@ defmodule Servy.Parser do
     %Conv{method: method, path: path, params: params, headers: headers}
   end
 
-  defp parse_headers([head | tail], headers) do
-    [key, value] = String.split(head, ": ")
-    headers = Map.put(headers, key, value)
-    parse_headers(tail, headers)
+  defp parse_headers(header_lines) do
+    Enum.reduce(header_lines, %{}, fn line, headers ->
+      [key, value] = String.split(line, ": ")
+      Map.put(headers, key, value)
+    end)
   end
-
-  defp parse_headers([], headers), do: headers
 
   defp parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim() |> URI.decode_query()
