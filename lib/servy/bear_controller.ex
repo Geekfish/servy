734048f376b3d47d7deb32b6ca29d2eb07f49fbf
Eleni Lixourioti <contact@eleni.co>
2019-04-11 18:37:40 +0100

    Add views with pre-compiled eex templates

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -3,25 +3,34 @@ require Logger
 defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
+  alias Servy.BearView
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
+  # @templates_path Path.expand("templates", File.cwd!())
+
+  # defp render(conv, template, bindings \\ []) do
+  #   content =
+  #     @templates_path
+  #     |> Path.join(template)
+  #     |> EEx.eval_file(bindings)
+
+  #   %{conv | status: 200, resp_body: content}
+  # end
+
+  defp render(conv, content) do
+    %{conv | status: 200, resp_body: content}
   end
 
   def index(conv) do
-    items =
+    bears =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join()
 
-    %{conv | status: 200, resp_body: "<ul>#{items}</ul>"}
+    render(conv, BearView.index(bears))
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
+    render(conv, BearView.show(bear))
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
