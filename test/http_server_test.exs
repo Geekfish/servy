f8abb8da230e6cb30422aa6a71e9b02772262085
Eleni Lixourioti <contact@eleni.co>
2019-04-13 16:20:11 +0100

    Replace custom client with HTTPPoison

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -4,22 +4,9 @@ defmodule HttpServerTest do
   test "Server returns bears" do
     spawn(Servy.HttpServer, :start, [4000])
 
-    request = """
-    GET /wildthings HTTP/1.1\r
-    Host: example.com\r
-    User-Agent: ExampleBrowser/1.0\r
-    Accept: */*\r
-    \r
-    """
+    {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
 
-    response = Servy.HttpClient.send_request(request)
-
-    assert response == """
-           HTTP/1.1 200 OK\r
-           Content-Type: text/html\r
-           Content-Length: 20\r
-           \r
-           Bears, Lions, Tigers
-           """
+    assert response.status_code == 200
+    assert response.body == "Bears, Lions, Tigers"
   end
 end
