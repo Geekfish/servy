a341d564a296cc334a27e10d4809624d473c833a
Eleni Lixourioti <contact@eleni.co>
2019-04-14 19:57:36 +0100

    Add get count of pledges

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -25,6 +25,11 @@ defmodule Servy.Handler do
     |> format_response
   end
 
+  def route(%Conv{method: "GET", path: "/404s"} = conv) do
+    counts = Servy.FourOhFourCounter.get_counts()
+    %{conv | status: 200, resp_body: "404s so far: #{inspect(counts)}"}
+  end
+
   def route(%Conv{method: "POST", path: "/pledges"} = conv) do
     Servy.PledgeController.create(conv, conv.params)
   end
