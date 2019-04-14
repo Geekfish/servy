448bc477445171444c8b9c485c33dc3169f7adc5
Eleni Lixourioti <contact@eleni.co>
2019-04-14 13:15:45 +0100

    Handle multiple async tasks example

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -4,9 +4,22 @@ defmodule HttpServerTest do
   test "Server returns bears" do
     spawn(Servy.HttpServer, :start, [4000])
 
-    {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
+    parent = self()
+    max_concurrent_requests = 5
 
-    assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
+    for _ <- 1..max_concurrent_requests do
+      spawn(fn ->
+        {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
+        send(parent, {:ok, response})
+      end)
+    end
+
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
   end
 end
