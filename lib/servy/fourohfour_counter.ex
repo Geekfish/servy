c252e0f19d703a05b53513e2562e3b7c9523d704
Eleni Lixourioti <contact@eleni.co>
2019-05-05 17:41:59 +0100

    Supervise the rest of the workers

--- lib/servy/fourohfour_counter.ex
+++ lib/servy/fourohfour_counter.ex
@@ -1,39 +1,43 @@
 defmodule Servy.FourOhFourCounter do
   @name __MODULE__
 
-  alias Servy.GenericServer
+  use GenServer
 
   # Client
 
-  def start() do
+  def start_link(_arg) do
     IO.puts("Starting the 404 server...")
-    GenericServer.start(__MODULE__, [], @name)
+    GenServer.start_link(__MODULE__, [], name: @name)
   end
 
   def bump_count(endpoint) do
-    Servy.GenericServer.cast(@name, {:bump_count, endpoint})
+    GenServer.cast(@name, {:bump_count, endpoint})
   end
 
   def get_count(endpoint) do
-    Servy.GenericServer.call(@name, {:get_count, endpoint})
+    GenServer.call(@name, {:get_count, endpoint})
   end
 
   def get_counts() do
-    Servy.GenericServer.call(@name, :get_counts)
+    GenServer.call(@name, :get_counts)
   end
 
   # Server Callbacks
+  def init(_init_arg) do
+    {:ok, %{}}
+  end
+
   def handle_cast({:bump_count, endpoint}, state) do
     state = Map.update(state, endpoint, 1, &(&1 + 1))
-    {state, state}
+    {:noreply, state}
   end
 
   def handle_call({:get_count, endpoint}, state) do
     count = Map.get(state, endpoint, 0)
-    {count, state}
+    {:reply, count, state}
   end
 
   def handle_call(:get_counts, state) do
-    {state, state}
+    {:reply, state, state}
   end
 end
